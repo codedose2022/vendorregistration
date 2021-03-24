7945c514be766de2f51429d0f7adf80acbd1320e
Dhanashri <sftdev16@mersatconsultants.com>
2021-03-24 15:32:36 +0400

    Tax Info Schema

---
+++ VendorRegistration/server/models/taxInfoSchema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const taxInfoSchema = mongoose.Schema(
+  {
+    comments:{
+      type:Object,
+      default:{}
+    },
+    vatNo: [String],
+    vatCopy: String,
+    tinNo: [Number],
+    status: {
+      type: String,
+      default:"saved",
+    },
+  },
+  { timestamps: true }
+);
+
+const taxInfo = mongoose.model("taxInfo", taxInfoSchema);
+
+export default taxInfo;
